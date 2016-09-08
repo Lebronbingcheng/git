b27d77cb765041a78764878e7d5fda50f219191b
wbc <1035515209@qq.com>
2016-09-08 11:12:00 +0800

    Graphv2.

--- Graph/src/graph/Graph.java
+++ Graph/src/graph/Graph.java
@@ -2,95 +2,165 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Graph {
-	private Node begin;
-	private List<Graph> list;
+	public int num;
+	public int[] nodeArray;
+	public int[][] edgeArray;
 	
-	public Graph(){
-		begin = null;
-		list = null;
+	public Graph(){}
+	public Graph(int num){
+		this.num = num;
+		nodeArray = new int[num];
 	}
 	
-	public Graph(Node node){
-		begin = node;
-		list = null;
+	public Graph(int...array){
+		this.num = array.length;
+		nodeArray = array;
+		edgeArray = new int[num][num];
 	}
-	public Graph(Node node, Graph...graphs){
-		list = new ArrayList<Graph>();
-		begin = node;
-		for(Graph g : graphs){
-			list.add(g);
+	
+	//建立结点之间的连接
+	public void creatEdge(int data1, int data2){
+		int index1 = findIndex(data1);
+		int index2 = findIndex(data2);
+		if(index1 != -1 && index2 != -1){
+			edgeArray[index1][index2] = 1;
+			edgeArray[index2][index1] = 1;
 		}
 	}
-	public Graph(Node node, Node...nodes){
-		list = new ArrayList<Graph>();
-		begin = node;
-		for(Node n : nodes){
-			Graph g = new Graph(n);
-			list.add(g);
-		}
+	
+	//delete an edge
+	public boolean deleteEdge(int data1, int data2){
+		int index1 = findIndex(data1);
+        int index2 = findIndex(data2);
+        if(index1 != -1 && index2 != -1 && edgeArray[index1][index2] == 1){
+			edgeArray[index1][index2] = 0;
+			edgeArray[index2][index1] = 0;
+			return true;
+        }	
+        return false;
 	}
 	
-	//Depth-First Traversal
-	public void DepthFirstTraversal(){
-		if(!begin.isStatus()){
-			begin.setStatus(true);
-			System.out.print(begin.getData()+" ");
-		}	
-		if(list != null){
-			for(Graph n : list){
-				n.DepthFirstTraversal();
+	//insert a node
+	public boolean insertNode(int data1, int data2){
+		int index = findIndex(data2);
+		if(index == -1){
+			return false;
+		}
+		int[] newArr = new int[num+1];
+		for(int i = 0,j = 0; i < nodeArray.length; i++,j++){
+			newArr[i] = nodeArray[j];
+		}
+		newArr[num] = data1;
+		nodeArray = newArr;
+		int[][] newEge = new int[num+1][num+1];
+		for(int i = 0,a = 0; i < edgeArray.length; i++, a++){
+			for(int j =0, b = 0; j < edgeArray[i].length; j++, b++){
+				newEge[i][j] = edgeArray[a][b];
 			}
 		}
+		edgeArray = newEge;
+		this.creatEdge(data1, data2);
+		return true;
 	}
 	
-	//Breadth-first Search Traversing 
-	/*public void BreadthFirstTraversal(){
-		System.out.print(begin.getData()+" ");
-		if(list != null){
-			for(Graph n : list){
-				n.begin.setStatus(true);
-				System.out.print(n.begin.getData() + " ");
+	//delete a node
+	public boolean deleteNode(int data){
+		if(findIndex(data) == -1){
+			return false;
+		}
+		List<Integer> l = new ArrayList<Integer>();
+		for(int a : nodeArray){
+			l.add(a);
+		}
+		int index = this.findIndex(data);
+		l.remove(index);
+		int[] newArr = new int[num-1];
+		for(int i = 0; i < newArr.length; i++){
+			newArr[i] = l.get(i);
+		}
+		nodeArray = newArr;
+		int[][] newEge = new int[num-1][num-1];
+		for(int i = 0, a = 0; i < index; i++,a++){
+			for(int j = 0, b = 0; j < index; j++, b++){
+				newEge[i][j] = this.edgeArray[a][b];
 			}
 		}
-	}*/
-	
-	//remove an edge
-	public boolean removeEdge(Graph a, Graph b){
-		return this.findGraph(a).list.remove(this.findGraph(b));
-	}
-	
-	//add an edge
-	public boolean addEdge(Graph a, Graph b){
-		return this.findGraph(a).list.add(this.findGraph(b));
-	}
-	
-	public Graph findGraph(Graph g){
-		if(this == g){
-			return this;
-		}
-		if(list != null){
-			for(Graph a : list){
-				return a.findGraph(g);
+		for(int i = index, a = index+1; a < num; i++,a++){
+			for(int j = 0, b = 0; j < index; j++, b++){
+				newEge[i][j] = this.edgeArray[a][b];
 			}
 		}
-		return null;
+		for(int i = 0, a = 0; a < index; i++,a++){
+			for(int j = index, b = index+1; b < num; j++, b++){
+				newEge[i][j] = this.edgeArray[a][b];
+			}
+		}
+		for(int i = index, a = index+1; a < num; i++,a++){
+			for(int j = index, b = index+1; b < num; j++, b++){
+				newEge[i][j] = this.edgeArray[a][b];
+			}
+		}
+		edgeArray = newEge;
+		return true;
 	}
-
-	public Node getBegin() {
-		return begin;
+	
+	private int findIndex(int data){
+		for(int i=0; i < nodeArray.length; i++){
+			if(nodeArray[i] == data){
+				return i;
+			}
+		}
+		return -1;
 	}
-
-	public void setBegin(Node begin) {
-		this.begin = begin;
+	
+	public boolean hasRelatedNode(int data){
+		if(findIndex(data) == -1 || findRelatedNode(data).isEmpty()){
+			return false;
+		}
+		return true;
 	}
-
-	public List<Graph> getList() {
-		return list;
+	
+	public Stack<Integer> findRelatedNode(int data){
+		int dataIndex = findIndex(data);
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int j = 0; j < edgeArray[dataIndex].length; j++){
+			if(edgeArray[dataIndex][j] == 1){
+				stack.push(nodeArray[j]);
+			}
+		}
+		return stack;
 	}
-
-	public void setList(List<Graph> list) {
-		this.list = list;
+	
+	//打印边数组
+	public void printEdgeArray(){
+		for(int i = 0; i<edgeArray.length; i++){
+			for(int j = 0; j < edgeArray[i].length; j++){
+				System.out.print(edgeArray[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int[] getNodeArray() {
+		return nodeArray;
+	}
+	public void setNodeArray(int[] nodeArray) {
+		this.nodeArray = nodeArray;
+	}
+	public int[][] getEdgeArray() {
+		return edgeArray;
+	}
+	public void setEdgeArray(int[][] edgeArray) {
+		this.edgeArray = edgeArray;
 	}
 }
--- Graph/src/test/Demo.java
+++ Graph/src/test/Demo.java
@@ -1,27 +1,19 @@
 package test;
 
 import graph.Graph;
-import graph.Node;
 
 public class Demo {
 	public static void main(String[] args){
-		Node[] nodes = new Node[9];
-		for(int i = 0; i < 9; i++){
-			nodes[i] = new Node(i);
-		}
-		Graph g7 = new Graph(nodes[7]);
-		Graph g6 = new Graph(nodes[6],g7);
-		Graph g3 = new Graph(nodes[3],g6,g7);
-		Graph g8 = new Graph(nodes[8]);
-		Graph g4 = new Graph(nodes[4],g8);
-		Graph g5 = new Graph(nodes[5],g8);
-		Graph g2 = new Graph(nodes[2],g4,g5);
-		Graph g1 = new Graph(nodes[1],g2,g3);
-		
-		g1.removeEdge(g4, g8);
-		g1.DepthFirstTraversal();
-		System.out.println();
-		g1.addEdge(g2, g8);
-		g1.DepthFirstTraversal();	
+		Graph g = new Graph(1,2,3,4,5);	
+		g.creatEdge(1, 4);
+		g.creatEdge(1, 5);
+		g.creatEdge(2, 4);
+		g.creatEdge(2, 5);
+	    g.creatEdge(3, 4);
+		//g.printEdgeArray();
+	    //g.insertNode(6, 5);
+	    //g.printEdgeArray();
+	    g.deleteNode(2);
+	    g.printEdgeArray(); 
 	}
 }
